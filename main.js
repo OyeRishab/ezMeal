c4b6894f698ebbe3090f90e800fe859694ad5d45
Rishab Jain <jainrishabbohra@gmail.com>
2023-04-08 21:32:50 +0530

    Smooth Scroll

--- main.js
+++ main.js
@@ -3,4 +3,25 @@ const navLinks = document.querySelector(".nav-links");
 
 menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("show-links");
+  menuBtn.classList.toggle("fa-xmark");
+});
+
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    navLinks.classList.remove("show-links");
+    menuBtn.classList.remove("fa-xmark");
+
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+
+    let position = element.offsetTop - 74;
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: "smooth",
+    });
+  });
 });
